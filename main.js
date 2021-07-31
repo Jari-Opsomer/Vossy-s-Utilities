e0619cbb43fb8924b88a02ba6bfadde976a9c49b
Jari-Opsomer <JariOpsomer@outlook.com>
2021-07-31 21:23:31 +0200

    init
    init

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ const bot = new discord.Client();
 bot.commands = new discord.Collection();
 bot.aliases = new discord.Collection();
 
+// When a member sends a message to the channel specified below we revoke his permission to write any further messages
+
+client.on('message', (message) => {
+
+    message.guild.channels.find(x => x.name === '<#870624822303010866>') //Replace it with the name of the channel for which you need this feature
+    
+    .overwritePermissions(message.author,{
+    
+    SEND_MESSAGES: false,
+    
+    })
+    
+    })
 
 bot.once('ready', () => {
     console.log('Bot is online!');
