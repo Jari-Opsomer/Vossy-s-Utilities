a39d45ddab718a29e0a0db3dd5942060310b78de
Jari-Opsomer <JariOpsomer@outlook.com>
2021-08-22 17:56:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,15 @@ client.on("message", async message => {
  
     if (command === `${prefix}help`) 
  
-        return message.channel.send("Command coming soon!");
+        return message.channel.send("`v!info` **Information about this bot.**\n`v!invite **The invite link for this bot.**`");
+
+    if (command === `${prefix}info`) 
+ 
+        return message.channel.send("This bot is made by **VossyGamer#1234**. More info coming soon!");
+
+    if (command === `${prefix}invite`) 
+ 
+        return message.channel.send("**At the moment the bot is private, but in the future the bot may become public.** Then the invite link will appear here!");
 
     if (command === `${prefix}`) 
 
